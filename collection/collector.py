245801179b64060f1a28fcf1390769e37e515f53
Eric Speelman <eric.t.speelman@gmail.com>
2019-11-18 17:41:32 -0500

    c

--- collection/collector.py
+++ collection/collector.py
@@ -4,20 +4,31 @@ import os.path
 
 cache_path = "../cache/"
 
-def collect(url, ignore_cache = False):
+def collect(url, ignore_cache = False, retry = 3):
     if not os.path.exists(cache_path):
         os.mkdir(cache_path)
     path = cache_path + url.replace('http://', '').replace('https://', '').replace('/', '_').replace('.', '_')
     html = None
+    cache_hit = False
     if not ignore_cache and os.path.exists(path):
-        file = open(path, 'r')
-        html = file.read()
-        file.close()
+        cache_hit = True
+        with open(path, 'r', encoding='utf-8') as file:
+            html = file.read()
     if html == None:
-        res = requests.get(url = "https://www.basketball-reference.com/")
+        try:
+            res = requests.get(url)
+        except:
+            if retry > 0:
+                collect(url, ignore_cache, retry - 1)
+            else:
+                raise Exception(f'An error occured while trying to retrieve {url}')
+        if res.status_code < 200 or res.status_code >= 300:
+            return None
         html = res.text
-    if not ignore_cache:
-        file = open(path, 'w')
-        file.write(html)
-        file.close()
+        html.replace('<!--', '').replace('-->', '')
+    if not ignore_cache and not cache_hit:
+        with open(path, 'w', encoding='utf-8') as file:
+            file.write(html)
+    if html == '':
+        return None
     return BeautifulSoup(html, 'html.parser')
--- collection/nba.py
+++ collection/nba.py
@@ -1,3 +1,90 @@
 from collector import collect
+import datetime
+import pandas as pd
+import csv
 
-collect('https://www.basketball-reference.com/leagues/NBA_2020_games-october.html')
+months = ['september', 'october', 'november', 'december', 'january', 'feburary', 'march', 'april', 'may']
+data_path = '../data/'
+player_data = []
+start_year = 2018
+end_year = 2020
+
+def collect_game(href):
+    print(f'Collecting https://www.basketball-reference.com{href}')
+    html = collect('https://www.basketball-reference.com' + href)
+    if html is None:
+        return []
+    game_data = {}
+    team_data = []
+    player_data = []
+    game_data['id'] = href.split('/')[2].split('.')[0]
+    game_scoreboard = html.select('div.scorebox > div')[2]
+    game_data['date'] = game_scoreboard.contents[1].contents[0]
+    if len(game_scoreboard.contents[2].contents) >= 1:
+        game_data['venue'] = game_scoreboard.contents[2].contents[0].split(',')[0]
+    else:
+        game_data['venue'] = ''
+    for team_html in html.select('div.scorebox > div')[:2]:
+        data = {}
+        team_data.append(data)
+        team_link = team_html.select('strong a')[0]
+        data['id'] = team_link['href'].split('/')[2]
+        data['name'] = team_link.contents[0]
+        data['points'] = int(team_html.select('div.score')[0].contents[0])
+        score_parts = list(map(lambda x: int(x), team_html.contents[4].contents[0].split('-')))
+        data['season_wins'] = score_parts[0]
+        data['season_losses'] = score_parts[1]
+    if team_data[0]['points'] > team_data[1]['points']:
+        team_data[0]['season_wins'] -= 1
+        team_data[1]['season_losses'] -= 1
+    else:
+        team_data[1]['season_wins'] -= 1
+        team_data[0]['season_losses'] -= 1
+    for team in team_data:
+        starting = 1
+        for stat_row in html.select(f'#box-' + team['id'] + '-game-basic tbody tr'):
+            data = {}
+            if stat_row.has_attr('class'):
+                starting = 0
+            elif len(stat_row.select('td')) > 1:
+                player_data.append(data)
+                player_link = stat_row.select('th a')[0]
+                data['id'] = player_link['href'].split('/')[3].split('.')[0]
+                data['name'] = player_link.contents[0]
+                data['starting'] = starting
+                for stat_cell in stat_row.select('td'):
+                    stat = stat_cell['data-stat']
+                    if not stat.endswith('pct') and stat != 'plus_minus':
+                        data[stat_cell['data-stat']] = stat_cell.contents[0]
+                for curr_team in team_data:
+                    prefix = 'team_'
+                    if curr_team['id'] != team['id']:
+                        prefix = 'opp_team_'
+                    for key in curr_team:
+                        data[prefix + key] = str(curr_team[key])
+                for key in game_data:
+                    data[f'game_{key}'] = str(game_data[key])
+    return player_data
+                
+            
+year = start_year
+while year < end_year:
+    for month in months:
+        html = collect(f'https://www.basketball-reference.com/leagues/NBA_{year}_games-{month}.html')
+        if html is not None:
+            for row in html.select('#schedule tr'):
+                th = row.select('th')[0]
+                if th.has_attr('csk'):
+                    links = row.select('a')
+                    if len(links) >= 4:
+                        href = row.select('a')[3]['href']
+                        player_data.extend(collect_game(href))
+
+    keys = player_data[0].keys()
+    with open(f'../data/nba_{year}.csv', 'w', encoding='utf-8', newline='') as file:
+        dict_writer = csv.DictWriter(file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(player_data)
+    year += 1
+    player_data = []
+
